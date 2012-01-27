91ad17989fb72dd08fb041941029de11e8457126
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-01-27 13:28:40 +0100

    Upgrade specs2

--- project/JdubextBuild.scala
+++ project/JdubextBuild.scala
@@ -10,13 +10,13 @@ object JdubextBuild extends Build
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
       "com.codahale" %% "jdub" % "0.0.6",
-      "org.specs2" %% "specs2" % "1.6.1"
+      "org.specs2" %% "specs2" % "1.7.1"
     ),
     scalacOptions += "-deprecation",
     scalacOptions += "-unchecked",
     publishTo := Some(Resolver.sftp(
       "iliaz",
       "scala.iliaz.com"
-    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa.pub"))
+    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa"))
   )
 }
