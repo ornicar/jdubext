33d99a9baf2f611e7679803fc42670a0fcf97ff2
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-12 16:07:54 +0200

    Bump version 1.9

--- project/JdubextBuild.scala
+++ project/JdubextBuild.scala
@@ -6,7 +6,7 @@ object JdubextBuild extends Build
   lazy val core = Project("core", file("./"), settings = Defaults.defaultSettings ++ Seq(
     organization:= "com.github.ornicar",
     name := "jdubext",
-    version := "1.8",
+    version := "1.9",
     scalaVersion := "2.9.1",
     libraryDependencies := Seq(
       "com.codahale" %% "jdub" % "0.0.6",
