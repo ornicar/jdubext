bd4b7868f520a28f0a2fa1410c366bfe4f17f893
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-04-12 12:30:08 +0200

    Implement multirow insertions

--- src/main/scala/jdubext/Sql.scala
+++ src/main/scala/jdubext/Sql.scala
@@ -1,6 +1,7 @@
 package com.github.ornicar.jdubext
 
 import com.codahale.jdub._
+import scalaz.{ NonEmptyList, NonEmptyLists }
 
 case class SQL(sql: String, values: Seq[Any] = Nil) extends Statement
 
@@ -17,17 +18,17 @@ object SQL {
    */
   def data(pairs: ((String, String), Any)*): Data = {
 
-    (Data(Nil, Nil, Nil) /: pairs.reverse) { (acc, pair) =>
+    (Data(Nil, Nil, Nil) /: pairs.reverse) { (acc, pair) ⇒
       val ((name, tpe), value) = pair
       val valueOrNull = value match {
-        case None | Some(None) | Some("") => null
-        case Some(x) => x
-        case x => x
+        case None | Some(None) | Some("") ⇒ null
+        case Some(x)                      ⇒ x
+        case x                            ⇒ x
       }
       valueOrNull match {
-        case null => acc
-        case v: Embed => acc.aggregate(name, v.sql, v.values)
-        case v => acc.aggregate(name, tpe, List(v))
+        case null     ⇒ acc
+        case v: Embed ⇒ acc.aggregate(name, v.sql, v.values)
+        case v        ⇒ acc.aggregate(name, tpe, List(v))
       }
     }
   }
@@ -46,7 +47,7 @@ object SQL {
 
     def values = valueSeq
 
-    def setters = nameSeq zip typeSeq map (a => a._1 + " = " + a._2) mkString ", "
+    def setters = nameSeq zip typeSeq map (a ⇒ a._1 + " = " + a._2) mkString ", "
 
     def insertIn(table: String) = SQL(
       "INSERT INTO %s (%s) VALUES (%s)".format(table, names, types),
@@ -56,6 +57,32 @@ object SQL {
       def where(condition: (String, Seq[Any])) = SQL(
         "UPDATE %s SET %s WHERE %s".format(table, setters, condition._1),
         values ::: condition._2.toList)
+    }
+
+    def +(other: Data): Multi = Multi(this, other)
+  }
+
+  case class Multi(datas: NonEmptyList[Data]) {
+
+    def +(other: Data): Multi = Multi(other, datas.list)
+
+    def insertIn(table: String) = SQL(
+      "INSERT INTO %s (%s) VALUES (%s)".format(
+        table,
+        datas.head.names,
+        datas.list map (_.types) mkString "), ("),
+      datas.list map (_.values) flatten
+    )
+  }
+
+  object Multi extends NonEmptyLists {
+
+    def apply(data: Data, others: Data*): Multi = apply(data, others.toList)
+
+    def apply(data: Data, others: List[Data]): Multi = new Multi(nel(data, others))
+
+    def apply(datas: List[Data]): Option[Multi] = datas.headOption map { head ⇒
+      apply(head, datas.tail)
     }
   }
 }
--- src/test/scala/jdubext/DatabaseSQLTest.scala
+++ src/test/scala/jdubext/DatabaseSQLTest.scala
@@ -18,30 +18,30 @@ class DatabaseSQLTest extends Specification {
         "name" -> "?" -> "John",
         "number" -> "?" -> 33
       ) mustEqual SQL.Data(
-        List("name", "number"),
-        List("?", "?"),
-        List("John", 33)
-      )
+          List("name", "number"),
+          List("?", "?"),
+          List("John", 33)
+        )
     }
     "process typed data" in {
       SQL.data(
         "name" -> "?::uuid" -> "John",
         "ip" -> "?::inet" -> "127.0.0.1"
       ) mustEqual SQL.Data(
-        List("name", "ip"),
-        List("?::uuid", "?::inet"),
-        List("John", "127.0.0.1")
-      )
+          List("name", "ip"),
+          List("?::uuid", "?::inet"),
+          List("John", "127.0.0.1")
+        )
     }
     "process simple and typed data" in {
       SQL.data(
         "name" -> "?" -> "John",
         "ip" -> "?::inet" -> "127.0.0.1"
       ) mustEqual SQL.Data(
-        List("name", "ip"),
-        List("?", "?::inet"),
-        List("John", "127.0.0.1")
-      )
+          List("name", "ip"),
+          List("?", "?::inet"),
+          List("John", "127.0.0.1")
+        )
     }
     "remove null values" in {
       SQL.data(
@@ -49,10 +49,10 @@ class DatabaseSQLTest extends Specification {
         "name" -> "?" -> "John",
         "foo" -> "?" -> null
       ) mustEqual SQL.Data(
-        List("name"),
-        List("?"),
-        List("John")
-      )
+          List("name"),
+          List("?"),
+          List("John")
+        )
     }
     "deal with embedded queries" in {
       val call = SQL.Embed("do_something(?, ?)", "foo" :: "bar" :: Nil)
@@ -61,10 +61,10 @@ class DatabaseSQLTest extends Specification {
         "query" -> "?" -> call,
         "color" -> "?" -> "black"
       ) mustEqual SQL.Data(
-        List("name", "query", "color"),
-        List("?", "do_something(?, ?)", "?"),
-        List("John", "foo", "bar", "black")
-      )
+          List("name", "query", "color"),
+          List("?", "do_something(?, ?)", "?"),
+          List("John", "foo", "bar", "black")
+        )
     }
   }
   "The insertIn function" should {
@@ -73,9 +73,9 @@ class DatabaseSQLTest extends Specification {
         "foo" -> "?" -> "bar",
         "foo2" -> "?::inet" -> "bar2"
       ) insertIn "baz" mustEqual SQL(
-        "INSERT INTO baz (foo, foo2) VALUES (?, ?::inet)",
-        "bar" :: "bar2" :: Nil
-      )
+          "INSERT INTO baz (foo, foo2) VALUES (?, ?::inet)",
+          "bar" :: "bar2" :: Nil
+        )
     }
   }
   "The updateIn function" should {
@@ -84,9 +84,23 @@ class DatabaseSQLTest extends Specification {
         "foo" -> "?" -> "bar",
         "foo2" -> "?::inet" -> "bar2"
       ) updateIn "baz" where ("id = ?", Seq(33)) mustEqual SQL(
-        "UPDATE baz SET foo = ?, foo2 = ?::inet WHERE id = ?",
-        "bar" :: "bar2" :: 33 :: Nil
-      )
+          "UPDATE baz SET foo = ?, foo2 = ?::inet WHERE id = ?",
+          "bar" :: "bar2" :: 33 :: Nil
+        )
+    }
+  }
+  "Multi insert" should {
+    "produce a valid query" in {
+      SQL.data(
+        "foo" -> "?" -> "bar",
+        "foo2" -> "?::inet" -> "bar2"
+      ) + SQL.data(
+          "foo" -> "?" -> "yummy",
+          "foo2" -> "?::inet" -> "yummy2"
+        ) insertIn "baz" mustEqual SQL(
+            "INSERT INTO baz (foo, foo2) VALUES (?, ?::inet), (?, ?::inet)",
+            "bar" :: "bar2" :: "yummy" :: "yummy2" :: Nil
+          )
     }
   }
 }
